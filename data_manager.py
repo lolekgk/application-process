14618db6ebe0d2d3625721cc6da5fc31d239ea5a
lolekgk <karol.gajda97@gmail.com>
2022-04-28 18:39:18 +0200

    Fix bad code practice

--- data_manager.py
+++ data_manager.py
@@ -18,21 +18,21 @@ def get_mentors(cursor):
 
 @database_common.connection_handler
 def get_mentors_by_last_name(cursor, last_name):
-    query = f"""
+    query = """
         SELECT first_name, last_name, city
         FROM mentor
-        ORDER BY last_name='{last_name}' DESC, first_name ASC"""
-    cursor.execute(query)
+        ORDER BY last_name=%(l_n)s DESC, first_name"""
+    cursor.execute(query, {'l_n': last_name})
     return cursor.fetchall()
 
 
 @database_common.connection_handler
 def get_mentors_by_city(cursor, city):
-    query = f"""
+    query = """
         SELECT first_name, last_name, city
         FROM mentor
-        WHERE city='{city}'
-        ORDER BY city='{city}'
+        WHERE city=%(city)s
+        ORDER BY city=%(city)s
         """
-    cursor.execute(query)
+    cursor.execute(query, {'city': city})
     return cursor.fetchall()

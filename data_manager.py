9a34f1ec0c0501eefe5a3f982867c5b5f79c0ded
lolekgk <karol.gajda97@gmail.com>
2022-04-28 16:49:38 +0200

    Add filtering by city

--- data_manager.py
+++ data_manager.py
@@ -24,3 +24,15 @@ def get_mentors_by_last_name(cursor, last_name):
         ORDER BY last_name='{last_name}' DESC, first_name ASC"""
     cursor.execute(query)
     return cursor.fetchall()
+
+
+@database_common.connection_handler
+def get_mentors_by_city(cursor, city):
+    query = f"""
+        SELECT first_name, last_name, city
+        FROM mentor
+        WHERE city='{city}'
+        ORDER BY city='{city}'
+        """
+    cursor.execute(query)
+    return cursor.fetchall()

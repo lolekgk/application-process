ab559c1f3f7c3d825229eb0c6e3149f688c3002d
lolekgk <karol.gajda97@gmail.com>
2022-04-29 12:35:27 +0200

    Update applicants_list function

--- main.py
+++ main.py
@@ -15,21 +15,20 @@ def index():
 def mentors_list():
     mentor_name = request.args.get('mentor-last-name')
     city = request.args.get('city-name')
-
     if mentor_name:
         mentor_details = data_manager.get_mentors_by_last_name(mentor_name)
     elif city:
         mentor_details = data_manager.get_mentors_by_city(city)
     else:
         mentor_details = data_manager.get_mentors()
-
     # We get back a list of dictionaries from the data_manager (for details check 'data_manager.py')
-
     return render_template('mentors.html', mentors=mentor_details)
 
 
-@app.route('/applicants')
+@app.route('/applicants', method=['GET', 'POST'])
 def applicants_list():
+    if request.method == 'POST':
+        data_manager.delete_applicant_by_email(request.form['email-ending'])
     applicants_details = data_manager.get_applicants()
     return render_template('applicants.html', applicants=applicants_details)
 
@@ -38,22 +37,18 @@ def applicants_list():
 def applicants_phone():
     applicant_name = request.form.get('applicant-name')
     applicant_email_ending = request.form.get('email-ending')
-
     if applicant_name:
         applicant_details = data_manager.get_applicant_data_by_name(applicant_name)
     else:
         applicant_details = data_manager.get_applicant_data_by_email_ending(applicant_email_ending)
-
     return render_template('applicant.html', applicants=applicant_details)
 
 
 @app.route('/applicants/<code>', methods=['GET', 'POST'])
 def edit_applicant_data(code):
-
     if request.method == 'POST':
         data_manager.update_applicant_phone_number(request.form['new-phone'], code)
         return redirect(url_for('edit_applicant_data', code=code))
-
     applicant_details = data_manager.get_applicant_data_by_code(code)
     return render_template('edit-applicant-data.html', code=code, applicants=applicant_details)
 
@@ -62,6 +57,8 @@ def edit_applicant_data(code):
 def delete_applicant(code):
     data_manager.delete_applicant_data(code)
     return redirect(url_for('applicants_list'))
+
+
 
 
 if __name__ == '__main__':
